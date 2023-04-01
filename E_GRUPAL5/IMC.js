1f4dc70b667792eee8bf2a160d726374f8939e94
Samuel Muñoz Tavernini <129233204+BLACKDOGG44@users.noreply.github.com>
2023-04-01 13:08:04 -0300

    Update IMC.js
    CAMBIO EN IMC

--- E_GRUPAL5/IMC.js
+++ E_GRUPAL5/IMC.js
@@ -1,11 +1,12 @@
-// CÓMO CALCULAR EL IMC (INDICE DE MASA CORPORAL)
+// CÓMO CALCULAR EL IMC (INDICE DE MASA CORPORAL
 function calcularIMC(peso, altura) {
-  var imc = peso / (altura * altura);
-  return imc.toFixed(2); // Redondeamos el resultado a dos decimales
-}
-
-// Ejemplo de uso
-var peso = 80; // en kilogramos
-var altura = 1.75; // en metros
-var imc = calcularIMC(peso, altura);
-console.log("El IMC es: " + imc);
+    var imc = peso / (altura * altura);
+    return imc.toFixed(2);
+  }
+  
+  var peso = parseFloat(prompt("Ingresa tu peso en kilogramos:"));
+  var altura = parseFloat(prompt("Ingresa tu altura en metros:"));
+  
+  var imc = calcularIMC(peso, altura);
+  
+  alert("Tu índice de masa corporal es: " + imc);
